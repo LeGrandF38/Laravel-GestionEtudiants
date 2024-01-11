0cf45f0526fc343520f484783ce4392c5c0fdeef
LeGrandF38 <faycalyacoubaprog@gmail.com>
2024-01-11 22:29:58 +0100

    5/6

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('/etudiants', [EtudiantController::class, 'index']);
